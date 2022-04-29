016eedbed91ff4c5642a391fbba1ea3fce62a145
thisgleam <73107276+thisgleam@users.noreply.github.com>
2022-04-29 15:15:26 +0700

    Welcome, Leave Webhooks & Unverified Role

---
+++ Events/Member/guildMemberAdd.js
@@ -0,0 +1,32 @@
+const { MessageEmbed, WebhookClient, GuildMember } = require("discord.js");
+const { ruleschannel, urlwelcomeleavelog, roleswelcome } = require("../../config.json");
+
+module.exports = {
+    name : "guildMemberAdd",
+    /**
+     * 
+     * @param {GuildMember} member 
+     */
+    execute(member) {
+        
+        const { user, guild } = member;
+
+        member.roles.add(roleswelcome)        
+        // https://discord.com/api/webhooks/111111111111111111/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        //                                   THIS ID            THIS TOKEN
+        const Welcomer = new WebhookClient({url: urlwelcomeleavelog});
+
+        const Welcome = new MessageEmbed()
+        .setColor("DARK_RED")
+        // .setAuthor({ name: user.tag, iconURL: user.avatarURL({dynamic: true, size: 512})})
+        .setAuthor({ name: "WELCOME", iconURL: user.avatarURL({dynamic: true, size: 512})})
+        .setThumbnail(user.avatarURL({dynamic: true, size: 512}))
+        .setDescription(`
+        Welcome to **${guild.name}**\nAccound Created: <t:${parseInt(user.createdTimestamp / 1000)}:R>\n
+        We kindly ask you to familiarize yourself with our rules over at <#${ruleschannel}>`)
+        .setFooter({ text: `Tag: ${user.tag} | ID: ${user.id} | Count: ${guild.memberCount}`});
+
+        //Welcomer.send({embeds: [Welcome]})
+        Welcomer.send({ content: `<@${user.id}>`, embeds: [Welcome]});
+    }
+}
---
+++ Events/Member/guildMemberRemove.js
@@ -0,0 +1,31 @@
+const { MessageEmbed, WebhookClient, GuildMember } = require("discord.js");
+const { urlwelcomeleavelog } = require("../../config.json");
+
+module.exports = {
+    name : "guildMemberRemove",
+    /**
+     * 
+     * @param {GuildMember} member 
+     */
+    execute(member) {
+        
+        const { user, guild } = member;
+        
+        // https://discord.com/api/webhooks/111111111111111111/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        //                                   THIS ID            THIS TOKEN
+        const Leaver = new WebhookClient({url: urlwelcomeleavelog});
+
+        const Leave = new MessageEmbed()
+
+        .setColor("DARK_RED")
+        .setAuthor({ name: "LEFT", iconURL: user.avatarURL({dynamic: true, size: 512})})
+        // .setAuthor({ name: user.tag, iconURL: user.avatarURL({dynamic: true, size: 512})})
+        .setThumbnail(user.avatarURL({dynamic: true, size: 512}))
+        .setDescription(`
+        ${member} has left the server\n
+        Joined: <t:${parseInt(member.joinedTimestamp / 1000)}:R>`)
+        .setFooter({ text: `Tag: ${user.tag} | ID: ${user.id} | Count: ${guild.memberCount}`});
+
+        Leaver.send({embeds: [Leave]})
+    }
+}
