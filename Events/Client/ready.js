cba5179aa2ef018b41b138ba58a007eb3587f36e
thisgleam <mylygleam3d@gmail.com>
2022-05-14 11:10:24 +0700

    another

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,6 +1,33 @@
 const { Client } = require("discord.js");
 const mongoose = require("mongoose");
 const { Database } = require("../../Structures/config.json");
+const osUtils = require("os-utils");
+const ms = require("ms");
+const DB = require('../../Structures/Schemas/ClientDB');
+
+// -------------- Events --------------//
+
+// Memory Data Update
+let memArray = [];
+
+setInterval(async () => {
+  //Used Memory in GB
+  memArray.push((osUtils.totalmem() - osUtils.freemem()) / 1024);
+
+  if (memArray.length >= 14) {
+    memArray.shift();
+  }
+
+  // Store in Database
+  await DB.findOneAndUpdate({
+      Client: true,
+    }, {
+      Memory: memArray,
+    }, {
+      upsert: true,
+    });
+}, ms("5s")); //= 5000 (ms)
+
 
 module.exports = {
     name: "ready",
